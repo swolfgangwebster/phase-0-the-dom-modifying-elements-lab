d0b9bd97fc1f35b39367dfbf375136b0dfa03029
Shawn Webster <swolfgangwebster@gmail.com>
2023-03-09 17:28:14 -0600

    who?

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory")
+newHeader.textContent = "Shawn is the champion"
